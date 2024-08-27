9bca5827f96170900a288404d6413a01c5a9f092
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-08-27 07:54:13 +0200

    Create randomize.js

---
+++ RandomizeJavascriptArrays/randomize.js
@@ -0,0 +1,22 @@
+
+line = [ 1, 2, 3, 4, 5 , 6 , 7, 8, 9, 10 ]
+
+// enrich the array with a function
+
+line.rand = () => {
+    const len    = line.length
+    const get_index = () => Number.parseInt( Math.random() * len )
+    for (let i = 0; i < 10; i++) {
+        let index_a = get_index()
+        let index_b = get_index()
+        if ( index_a != index_b ) {
+            let values = [ line[index_a], line[index_b] ]
+            line[index_a] = values[1]
+            line[index_b] = values[0]
+        }    
+    }
+}
+
+// then simply call the function. No matter, what is in the array it will be randomized
+
+line.rand()
