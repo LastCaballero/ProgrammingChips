25be4cdca3e4f8aac882af06a749ab08bdadb0f3
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-09-15 13:10:36 +0200

    Create Juggle.pm

---
+++ Modules/Juggle.pm
@@ -0,0 +1,17 @@
+package Juggle ;
+
+use strict      ;
+use warnings    ;
+
+sub juggle_array {
+    my $ar_ref = shift ;
+    my( $rd1, $rd2 , $mem ) ;
+    my $rand = sub { int( rand( @$ar_ref + 0 ) ) } ;
+    for ( 1 .. 300 ) {
+        $rd1 = &$rand ;
+        $rd2 = &$rand ;
+        $mem = $ar_ref->[$rd1] ;
+        $ar_ref->[$rd1] = $ar_ref->[$rd2] ;
+        $ar_ref->[$rd2] = $mem ; 
+    }
+}
