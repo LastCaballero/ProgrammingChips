0e9744ae5f6fbb4e5104a66fa1585eb4d1c567d0
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-09-15 13:41:43 +0200

    Update line_count.pl

--- linux/line_count.pl
+++ linux/line_count.pl
@@ -1,19 +1,21 @@
-#! /usr/bin/perl
-
 use strict      ;
 use warnings    ;
 
-
+my @sorted_list ;
 my @files   = grep { -e }  @ARGV    ;
 my @stdi    = <stdin> if ( not -t ) ;
 
 if ( @stdi ) {
-    printf "%-20s %4d\n",'stdin' , scalar @stdi ;
+    push @sorted_list, [ 'stdin', scalar @stdi ] ;
 }
 
 for ( @files ) {
     open my $file , '<' ,   $_ ;
     my      @lines  =   <$file> ;
     close   $file ;
-    printf "%-20s %4d\n" ,  $_ ,    scalar @lines ;
+    push @sorted_list, [ $_ , scalar @lines ] ;
+}
+
+for( sort { $a->[1] <=> $b->[1] } @sorted_list ) {
+    printf "%-30s %4d\n" ,$_->[0] , $_->[1] ;
 }
