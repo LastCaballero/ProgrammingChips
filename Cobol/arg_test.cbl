9525d7fa87bc1065a1efb95636cc815b162e5eb7
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-10-17 09:49:30 +0200

    Create arg_test.cbl

---
+++ Cobol/arg_test.cbl
@@ -0,0 +1,13 @@
+identification 	division.
+program-id.	'argument test'.
+
+data division.
+	working-storage section.
+	01 	arg1	pic 	X(200).
+
+
+procedure division.
+	accept		arg1.
+	display 	arg1.
+
+end-run.
